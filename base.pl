99a9b9e4c21509ee075303a2e71eeeb51d86e5f4
Maximiliano Valenzuela <maximiliano.valenzuela.s@usach.cl>
2022-04-28 18:33:45 -0400

    Agregar ejemplos a la base de conocimientos

--- base.pl
+++ base.pl
@@ -1,13 +1,27 @@
 % Game - Formato: game(nombreJuego,Categoria,Duracion,Antiguedad,Dificultad,CaractericaExtra)
+%Juegos de plataforma
 game('Donkey Kong',plataforma,dur_media,antiguo,dif_media,etapas_distintas).
 game('Crash Bandicoot',plataforma,dur_media,antiguo,dif_facil,etapas_distintas).
 game('Cuphead',plataforma,dur_media,nuevo,dif_dificil,etapas_distintas).
+game('Super Mario world',plataforma,dur_media,antiguo,dif_media,etapas_distintas).
+game('Sonic The Hedgehog',plataforma,dur_corta,antiguo,dif_facil,etapas_distintas).
+
+%Juegos de puzzle
 game('Portal',puzzle,dur_corta,antiguo,dif_media,pensar).
 game('Portal 2',puzzle,dur_media,nuevo,dif_media,pensar).
 game('It takes two',puzzle,dur_larga,nuevo,dif_media,pensar).
+game('Superliminal',puzzle,dur_corta,nuevo,dif_dificil,pensar).
+game('Hitman:Codename 47',puzzle,dur_corta,antiguo,dif_facil,pensar).
+
+
+%Juegos sandbox
 game('Minecraft',sandbox,dur_media,antiguo,dif_facil,mundo_abierto).
 game('Grand Theft Auto V',sandbox,dur_larga,nuevo,dif_facil,mundo_abierto).
 game('Grand Theft Auto San Andreas',sandbox,dur_larga,antiguo,dif_media,mundo_abierto).
+game('Elden Ring',sandbox,dur_corta,nuevo,dif_dificil,mundo_abierto).
+game('Assasins Creed',sandbox,dur_larga,nuevo,dif_media,mundo_abierto).
+
+%Juegos roguelike
 game('Muck',roguelike,dur_corta,nuevo,dif_dificil,sin_historia).
 game('Muck1',roguelike,dur_corta,nuevo,dif_dificil,sin_historia).
 game('Muck2',roguelike,dur_corta,nuevo,dif_dificil,sin_historia).
@@ -16,3 +30,5 @@ game('Muck4',roguelike,dur_corta,nuevo,dif_dificil,sin_historia).
 game('Muck5',roguelike,dur_corta,nuevo,dif_dificil,sin_historia).
 game('The Binding of Isaac Repentance',roguelike,dur_larga,nuevo,dif_media,sin_historia).
 game('Hades',roguelike,dur_corta,nuevo,dif_media,sin_historia).
+game('Enter the Gungeon',roguelike,dur_corta,nuevo,dif_facil,sin_historia).
+game('Risk of Rain',roguelike,dur_corta,nuevo,dif_media,sin_historia).
