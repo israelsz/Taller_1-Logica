eb4021ffecb4e3e52af068139f4aaed63c322dcb
Maximiliano Valenzuela <maximiliano.valenzuela.s@usach.cl>
2022-04-28 23:53:30 -0400

    crear programa que consume la api de speed run para poblar la base de conocimiento

--- base.pl
+++ base.pl
@@ -1,4 +1,42 @@
-% Game - Formato: game(nombreJuego,Categoria,Duracion,Antiguedad,Dificultad,CaractericaExtra)
+%Representación de un videojuego registrado en la base de datos que puede
+% permitir obtener una sugerencia de género de videojuegos para el usuario
+%Formato: game(nombre_juego,genero, duración,antigüedad,dificultad,característica_extra)
+% nombre_juego: String que indica el nombre de un videojuego, además puede ir
+%               acompañado por un guión seguido de una categoría de speedrun
+%               en el caso de que se quiere especificar un juego y su
+%               categoría
+% genero:       átomo que representa el genero/tipo de juego al cual corresponde el
+%               el videojuego. Para la base la implementación se toman en cuenta los
+%               generos representados por los átomos:
+%               -puzzle
+%               -plataforma
+%               -sandbox
+%               -metroidvania
+% duracion:     átomos que representan la duracion de la run que posea word record en
+%               speedrun para el juego en cuestion. Para la base de conocimiento se
+%               toma en cuenta los tiempos representados por los átomos:
+%               -dur_corta = duracion de la run menor a 30 minutos
+%               -dur_media = duracion de la run entre 30 minutos y 90 minutos
+%               -dur_larga = duracion de la run mayor a 90 minutos
+% antiguedad:   atomos que representan la antiguedad del videojuego dependiendo su fecha
+%               de lanzamiento. Para la base de conocimiento se consideran las antiguedades
+%               -antiguo: juegos con fecha de lanzamiento anterior al 2000
+%               -nuevo: juegos con fecha de lanzamiento posterior al 2000
+% dificultad:   atomos que representan la dificultad del videojuego y de la run dependiendo
+%               la cantidad de jugadores?? que activos intentando realizar la run. Los
+%               atomos considerados para la base de conocimiento son:
+%               -dif_media
+%               -dif_facil
+%               -dif_dificil
+% caracteristica_extra: corresponde a alguna caracteristica propia del genero que puede
+%               ayudar a determinar cual es la preferencia del usuario. Las
+%               caracteristicas presentes en la base de conocimiento son
+%               -etapas_distintas
+%               -pensar
+%               -mundo_abierto
+%               -desbloquear_zonas
+%
+
 %Juegos de plataforma
 game('Donkey Kong',plataforma,dur_media,antiguo,dif_media,etapas_distintas).
 game('Crash Bandicoot',plataforma,dur_media,antiguo,dif_facil,etapas_distintas).
